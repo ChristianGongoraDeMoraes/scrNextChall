2a2f4ba4bc22fe57e2701bc875ce2b78bcb48c45
chris <christianpimpo304@hotmail.com>
2025-10-02 04:48:55 -0300

    chart texts fixed

--- src/app/calculadora/Chart.tsx
+++ src/app/calculadora/Chart.tsx
@@ -8,14 +8,36 @@ import {
   CartesianGrid,
 } from "recharts"
 import { LinhaResultadoProps } from "./LinhaResultado"
+import { useEffect, useState } from "react";
 
 export interface ChartProps{
     resultados: LinhaResultadoProps[];
     setShowGrafico: (arg: boolean) => void;
 }
+interface ChartArray{
+    Total: number,
+    Rendimento_dos_Juros: number,
+    Total_Investido: number,
+    mes: number
+}
 
 export default function ChartGanhos(props: ChartProps) {
-  return (
+    const [arrayFomatado, setArrayFormatado] = useState<ChartArray[]>([]);
+
+    useEffect(() => {
+        const arrayLoop: ChartArray[] = [];
+        props.resultados.map(i => {
+            arrayLoop.push({
+                Total: Number(i.total_acumulado.toFixed(2)),
+                Rendimento_dos_Juros: Number(i.rendimento_acumulado.toFixed(2)),
+                Total_Investido: Number(i.acumulado.toFixed(2)),
+                mes: i.mes
+            });
+        });
+        setArrayFormatado(arrayLoop);
+    }, [props.resultados]);
+    
+    return (
     <div className="fixed w-full inset-0 bg-black bg-opacity-70 flex justify-center flex-wrap items-center z-50 overflow-y-auto ">
         <div className="bg-gray-200 rounded-lg p-3 w-[90%] min-h-[90vh] relative mt-10">
             <button
@@ -31,14 +53,29 @@ export default function ChartGanhos(props: ChartProps) {
             </div>
             <div className="p-2">
                 <div className="w-full h-[400px]">
-                <ResponsiveContainer width="100%" height="100%">
-                    <LineChart data={props.resultados}>
-                    <CartesianGrid strokeDasharray="12 12" className="stroke-gray-300 dark:stroke-gray-600" />
+                <div className="flex flex-col text-black items-center justify-center w-full font-bold">
+                    <div className="flex items-center justify-center">
+                        <div className="bg-red-500 h-3 w-3 rounded-full"></div>
+                        <p>Total</p>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <div className="rounded bg-blue-500 h-3 w-3"></div>
+                        <p>Rendimento dos Juros</p>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <div className="rounded bg-green-500 h-3 w-3"></div>
+                        <p>Total Investido</p>
+                    </div>
+                </div>
+                <ResponsiveContainer width="100%" height="100%" className={"text-black"}>
+                    <LineChart data={arrayFomatado}>
+                    <CartesianGrid strokeDasharray="12 12" className="stroke-black" />
                     <XAxis dataKey="mes" stroke="#000000ff" />
                     <YAxis stroke="#000000ff" />
-                    <Tooltip />
-                    <Line type="monotone" dataKey="rendimento_acumulado" stroke="#2563eb" strokeWidth={2} />
-                    <Line type="monotone" dataKey="acumulado" stroke="#46eb25" strokeWidth={2} />
+                    <Tooltip/>
+                    <Line type="monotone" dataKey="Rendimento_dos_Juros" stroke="#0011ffff" strokeWidth={2} />
+                    <Line type="monotone" dataKey="Total_Investido" stroke="#46eb25" strokeWidth={2} />
+                    <Line type="monotone" dataKey="Total" stroke="#ff0000ff" strokeWidth={2} />
                     </LineChart>
                 </ResponsiveContainer>
                 </div>
